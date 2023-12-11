41b74c6100459b1fb6076b5dd6e9d060008e2b2a
Mali <keeroh@gmail.com>
2023-12-11 15:11:42 +0500

    app components

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,46 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import { SafeAreaView, ScrollView, StyleSheet } from "react-native";
+import * as React from "react";
+import EditScreenInfo from "../../components/EditScreenInfo";
+import { Text, View } from "../../components/Themed";
+import Lottie from "lottie-react-native";
+import { RefreshControl } from "react-native-gesture-handler";
+import { useRef } from "react";
+import { ThreadsContext } from "../../context/thread-context";
+import ThreadItem from "../../components/ThreadItem";
 
 export default function TabOneScreen() {
+  const animationRef = useRef<Lottie>(null);
+  const threads = React.useContext(ThreadsContext);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+    <SafeAreaView>
+      <ScrollView
+        contentContainerStyle={{
+          // backgroundColor: "grey",
+          paddingTop: 55,
+          paddingHorizontal: 10,
+        }}
+        refreshControl={
+          <RefreshControl
+            refreshing={false}
+            onRefresh={() => animationRef.current?.play()}
+            tintColor={"transparent"}
+          />
+        }
+      >
+        <Lottie
+          ref={animationRef}
+          source={require("../../assets/threads.json")}
+          loop={false}
+          autoPlay
+          style={{ width: 90, height: 90, alignSelf: "center" }}
+          // onAnimationFinish={() => alert("finished")}
+        />
+
+        {threads.map((thread) => (
+          <ThreadItem key={thread.id} {...thread} />
+        ))}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
